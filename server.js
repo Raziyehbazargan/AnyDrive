bcda894714885d195aa2ba58d7d05936dfd69e31
raziyeh bazargan <r.bazargan@gmail.com>
2017-03-01 15:42:21 -0800

    delete bucket is working

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const express = require('express');
 
 //const errorMiddleware = require('./src/lib/error-middleware');
 //const authRouter = require('./src/route/auth-router');
-const photoAwsRouter = require('./src/route/photo-aws-router');
+const photoAwsRouter = require('./src/route/aws-photo-router');
+const bucketAwsRouter = require('./src/route/aws-bucket-router');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.use(morgan('dev'));
 
 //app.use(authRouter);
 app.use(photoAwsRouter);
+app.use(bucketAwsRouter);
 //app.use(errorMiddleware);
 
 const server = module.exports = app.listen(PORT, function() {
---
+++ src/route/aws-bucket-router.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// NPM MODULES
+const AWS = require('aws-sdk');
+const createError = require('http-errors');
+const debug = require('debug')('AnyDrive:aws-bucket-router');
+
+//APP MODULES
+const Photo = require('../model/photo');
+//const bearerAuth = require('../lib/bearer-auth-middleware');
+const s3Promisify = require('../lib/aws-promisify');
+
+// module config
+AWS.config = new AWS.Config();
+AWS.config.setPromisesDependency(require('bluebird'));
+
+// MODULE CONSTANTS
+const s3 = new AWS.S3();
+const dataDir =`${__dirname}/../data`;
+const bucketAwsRouter = module.exports = require('express').Router();
+
+bucketAwsRouter.post('/api/bucket/:bucket/create', function(req, res, next) {
+  if (!req.params.bucket) next(createError(400, 'no bucket name'));
+  return s3Promisify.s3CreateBucket(req.params.bucket)
+  .then(bucketName => res.send(bucketName))
+  .catch(err => next(createError(400, 'no successful')));
+});
+
+bucketAwsRouter.get('/api/bucket/buckets', function(req, res, next) {
+  return s3Promisify.s3ListBuckets()
+  .then(data => res.send(data))
+  .catch(err => next(createError(400, 'no successful')));
+});
+
+bucketAwsRouter.delete('/api/bucket/:bucket/delete', function(req, res, next) {
+  if (!req.params.bucket) next(createError(400, 'no bucket name'));
+  return s3Promisify.s3DeleteBucket({Bucket: req.params.bucket})
+  .then(result => res.send(result))
+  .catch(err => next(createError(400, 'no successful')));
+});
