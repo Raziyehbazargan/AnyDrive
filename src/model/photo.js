67b6fa722cafa3488ebc60de0e0f66ee88e13e16
raziyeh bazargan <r.bazargan@gmail.com>
2017-03-06 17:25:05 -0800

    declare a variable for Schema instead of using mongoose.Schema

--- src/model/photo.js
+++ src/model/photo.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const photoSchema = mongoose.Schema({
+const photoSchema = Schema({
   name: {type: String},
   caption: {type: String},
   imageURI: {type: String, required: true},
   objectKey: {type: String, required: true},
-  userID: {type: mongoose.Schema.Types.ObjectId},
+  userID: {type: Schema.Types.ObjectId},
+  galleryID: { type: Schema.Types.ObjectId, required: true },
   created: {type: Date, default: Date.now},
 });
 
