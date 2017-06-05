65120f0a163abee7d19c73bcdc0b8322198f309d
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-06-04 19:58:33 -0700

    initial react

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,41 @@
-'use strict';
-
-require('dotenv').load();
-
-const webpack = require('webpack');
-const HTMLPlugin = require('html-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const production = process.env.NODE_ENV === 'production';
-
-let plugins = [
-  new ExtractTextPlugin('bundle.css'),
-  new HTMLPlugin({ template: `${__dirname}/app/index.html` }),
-  new webpack.DefinePlugin({
-    __API_URL__: JSON.stringify(process.env.API_URL),
-    __GOOGLE_CLIENT_ID__: JSON.stringify(process.env.GOOGLE_CLIENT_ID),
-    __TITLE__: JSON.stringify(process.env.TITLE),
-    __DEBUG__: JSON.stringify(!production),
-  }),
-];
-
-if (production){
-  plugins = plugins.concat([
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-      compress: {
-        warnings: false,
-      },
-    }),
-    new CleanPlugin(),
-  ]);
-}
+const webpack = require("webpack");
+const path = require('path')
 
 module.exports = {
-  entry: `${__dirname}/app/entry.js`,
-  devtool: production ? false : 'eval',
-  plugins,
-  output: {
-    path: 'build',
-    filename: 'bundle.js',
-  },
-  sassLoader: {
-    includePaths: [`${__dirname}/app/scss/lib`],
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-      },
-      {
-        test: /\.html$/,
-        loader: 'html',
-      },
-      {
-        test: /\.(woff|ttf|svg|eot).*/,
-        loader: 'url?limit=10000&name=font/[name].[ext]',
-      },
-      {
-        test: /\.(jpg|jpeg|bmp|tiff|gif|png)$/,
-        loader: 'url?limit=10000&name=image/[hash].[ext]',
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css!resolve-url!sass?sourceMap'),
-      },
-    ],
-  },
-};
+	entry: "./app/entry.js",
+	output: {
+		path: "./build",
+		filename: "bundle.js",
+		publicPath: "build"
+	},
+	devServer: {
+		inline: true,
+		contentBase: `${__dirname}/app`,
+		port: 4000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: ["babel-loader"],
+				query: {
+					presets: ["latest", "stage-0", "react"]
+				}
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+				loader: "json-loader"
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader'
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+			}
+		]
+	}
+}
