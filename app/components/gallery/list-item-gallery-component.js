b4ad44e18e44c9b5de5b5f2f88b29523da868025
raziyeh bazargan <r.bazargan@gmail.com>
2017-07-19 23:36:39 -0700

    add remove button for eac listitem

--- app/components/gallery/list-item-gallery-component.js
+++ app/components/gallery/list-item-gallery-component.js
@@ -1,12 +1,19 @@
 import { Component } from 'react';
-import { Form, FormControl , FormGroup, Col, Button, ControlLabel, Checkbox, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Form, FormControl , Glyphicon, FormGroup, Col, Button, ControlLabel, Checkbox, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
 import request from 'superagent';
 
 const APP_URI = 'http://localhost:3000';
 
 const ListItemGallery = ({index, gallery}) => {
   return (
-      <ListGroupItem key={index}>{gallery.name}</ListGroupItem>
+      <div>
+        <ListGroupItem key={index}>{gallery.name}
+        <Button className="pull-right" bsStyle="danger" bsSize="small">
+          <Glyphicon glyph="trash" />
+        </Button>
+        </ListGroupItem>
+
+      </div>
   );
 };
 
