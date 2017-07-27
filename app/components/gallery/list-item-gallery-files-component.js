b9321cce985e7caf14cf7e91eb26f9701c141d12
raziyeh bazargan <r.bazargan@gmail.com>
2017-07-26 23:15:33 -0700

    adding upload file - used Dropzone package

--- app/components/gallery/list-item-gallery-files-component.js
+++ app/components/gallery/list-item-gallery-files-component.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import { Form, FormControl , FormGroup, Col, Button, ControlLabel, Checkbox, Image } from 'react-bootstrap';
 import request from 'superagent';
-
-import SideMenu from './side-menu-component';
+import Dropzone from 'react-dropzone';
+//import SideMenu from './side-menu-component';
 const APP_URI = process.env.APP_URI || 'http://localhost:3300';
 
 
@@ -11,22 +12,22 @@ class ListItemGalleryFiles extends Component {
     this.state = {
       galleryId: null,
     };
+    this.uploadFile = this.uploadFile.bind(this);
+  }
+  uploadFile(file) {
+    console.log('Upload File:');
+
   }
   componentDidMount() {
     request
       .get(`${APP_URI}/api/gallery/name/${this.props.match.params.galleryname}`)
       .then(gallery => {
-        console.log('iddd-->', gallery, this.props.match.params.galleryname)
         this.setState({ galleryId: gallery.body[0]._id});
       });
   }
   render() {
-    const { query } = this.props.location;
-    const { params } = this.props;
-    //console.log(this.props.match.params.galleryname);
-
     return (
-        <h1>heey {this.state.galleryId}</h1>
+      <Dropzone onDrop={this.uploadFile} />
     );
   }
 }
